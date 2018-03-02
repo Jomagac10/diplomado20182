aa1740e36ee67cd15eed4b54504d39433b3d131b
Jose Manuel García Chávez <jomagac_10@hotmail.com>
2018-03-02 04:15:57 -0600

    Added Exercise chapter 3

---
+++ XcodeProjects/Exercise2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, swift"
+
+let consrStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+
+var arrayOfInts: Array<Int>
+var arrayOfInts2: [Int]
+
