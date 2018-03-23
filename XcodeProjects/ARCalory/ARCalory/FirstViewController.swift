c211726fde6204370bd7507ce51baa1ddbd437ae
Jose Manuel García Chávez <jomagac_10@hotmail.com>
2018-03-23 03:14:25 -0600

    Added characteristics to my app and navigation with launchscreen project to my app

---
+++ XcodeProjects/ARCalory/ARCalory/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  ARCalory
+//
+//  Created by Jose Manuel García Chávez on 3/23/18.
+//  Copyright © 2018 Jose Manuel García Chávez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func UnwinedToVerMas (UnwidSegue: UIStoryboardSegue){
+        
+    }
+
+
+}
+
---
+++ XcodeProjects/ARCalory/ARCalory/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ARCalory
+//
+//  Created by Jose Manuel García Chávez on 3/23/18.
+//  Copyright © 2018 Jose Manuel García Chávez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
