010c2f9a4fbcc37de4fd989e2411ce54a19879b9
Jose Manuel García Chávez <jomagac_10@hotmail.com>
2018-06-10 01:29:31 -0500

    Added chapter 14

--- XcodeProjects/Homepwner/Homepwner/ItemsViewController.swift
+++ XcodeProjects/Homepwner/Homepwner/ItemsViewController.swift
@@ -11,13 +11,7 @@ import UIKit
 class ItemsViewController: UITableViewController{
     var itemStore: ItemStore!
     
-    @IBAction func addNewItem(_ sender: UIButton) {
-//        // Make a new index path for the 0th section, last row
-//        let lastRow = tableView.numberOfRows(inSection: 0)
-//        let indexPath = IndexPath(row: lastRow, section: 0)
-//        // Insert this new row into the table
-//        tableView.insertRows(at: [indexPath], with: .automatic)
-        
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
         // Create a new item and add it to the store
         let newItem = itemStore.createItem()
         // Figure out where that item is in the array
@@ -27,37 +21,14 @@ class ItemsViewController: UITableViewController{
             tableView.insertRows(at: [indexPath], with: .automatic)
         }
     }
-    @IBAction func toggleEditingMode(_ sender: UIButton) {
-        // If you are currently in editing mode...
-        if isEditing {
-            // Change text of button to inform user of state
-            sender.setTitle("Edit", for: .normal)
-            // Turn off editing mode
-            setEditing(false, animated: true)
-        } else {
-            // Change text of button to inform user of state
-            sender.setTitle("Done", for: .normal)
-            // Enter editing mode
-            setEditing(true, animated: true)
-        }
-    }
     
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        // Create an instance of UITableViewCell, with default appearance
-//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
-        // Get a new or recycled cell
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
-//                                                 for: indexPath)
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",
                                                  for: indexPath) as! ItemCell
-        // Set the text on the cell with the description of the item
-        // that is at the nth index of items, where n = row this cell
-        // will appear in on the tableview
         let item = itemStore.allItems[indexPath.row]
-//        cell.textLabel?.text = item.name
-//        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        // Configure the cell with the Item
+
         cell.nameLabel.text = item.name
         cell.serialNumberLabel.text = item.serialNumber
         cell.valueLabel.text = "$\(item.valueInDollars)"
@@ -120,15 +91,18 @@ class ItemsViewController: UITableViewController{
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Get the height of the status bar
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
-        
-//        tableView.rowHeight = 65
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
     }
