62379b5020300b2bb115141e9350e11e87a3bb25
Jose Manuel García Chávez <jomagac_10@hotmail.com>
2018-06-27 01:07:07 -0500

    Added chapter 16

--- XcodeProjects/Homepwner/Homepwner/ItemStore.swift
+++ XcodeProjects/Homepwner/Homepwner/ItemStore.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ItemStore{
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories =
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.archive")
+    }()
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path)")
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path)
+    }
+    
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -17,11 +29,12 @@ class ItemStore{
         return newItem
     }
     
-//    init() {
-//        for _ in 0..<5 {
-//            createItem()
-//        }
-//    }
+    init() {
+        if let archivedItems =
+            NSKeyedUnarchiver.unarchiveObject(withFile: itemArchiveURL.path) as? [Item] {
+            allItems = archivedItems
+        }
+    }
     
     func removeItem(_ item: Item) {
         if let index = allItems.index(of: item) {
@@ -39,5 +52,7 @@ class ItemStore{
         // Insert item in array at new location
         allItems.insert(movedItem, at: toIndex)
     }
+    
+    
     
 }
